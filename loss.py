0129a0570a174bd3088a09e5a3901acb69d35b71
zjk0803 <43437186+zjk0803@users.noreply.github.com>
2018-12-11 22:20:59 +0800

    Update loss.py

--- loss.py
+++ loss.py
@@ -54,3 +54,17 @@ max(0, 4.3) + max(0, 0.9)
 5.2 (High loss as very wrong prediction)
 
 
+#交叉熵损失/负对数似然：
+import numpy as np
+predictions = np.array([[0.25,0.25,0.25,0.25],
+                        [0.01,0.01,0.01,0.96]])
+targets = np.array([[0,0,0,1],
+                   [0,0,0,1]])
+def cross_entropy(predictions, targets, epsilon=1e-10):
+    predictions = np.clip(predictions, epsilon, 1. - epsilon)
+    N = predictions.shape[0]
+    ce_loss = -np.sum(np.sum(targets * np.log(predictions + 1e-5)))/N
+    return ce_loss
+cross_entropy_loss = cross_entropy(predictions, targets)
+print ("Cross entropy loss is: " + str(cross_entropy_loss))
+
