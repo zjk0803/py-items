18dceffc7fc4dac0b88f48fd537d5aef48264b74
zjk0803 <43437186+zjk0803@users.noreply.github.com>
2018-12-11 22:19:20 +0800

    Update loss.py

--- loss.py
+++ loss.py
@@ -1,1 +1,33 @@
 
+#均方误差/平方损失/L2 损失
+import numpy as np
+y_hat = np.array([0.000, 0.166, 0.333])
+y_true = np.array([0.000, 0.254, 0.998])
+def rmse(predictions, targets):
+    differences = predictions - targets
+    differences_squared = differences ** 2
+    mean_of_differences_squared = differences_squared.mean()
+    rmse_val = np.sqrt(mean_of_differences_squared)
+    return rmse_val
+print("d is: " + str(["%.8f" % elem for elem in y_hat]))
+print("p is: " + str(["%.8f" % elem for elem in y_true]))
+rmse_val = rmse(y_hat, y_true)
+print("rms error is: " + str(rmse_val))
+
+
+#平均绝对误差/L1 损失
+import numpy as np
+y_hat = np.array([0.000, 0.166, 0.333])
+y_true = np.array([0.000, 0.254, 0.998])
+
+print("d is: " + str(["%.8f" % elem for elem in y_hat]))
+print("p is: " + str(["%.8f" % elem for elem in y_true]))
+
+def mae(predictions, targets):
+    differences = predictions - targets
+    absolute_differences = np.absolute(differences)
+    mean_absolute_differences = absolute_differences.mean()
+    return mean_absolute_differences
+mae_val = mae(y_hat, y_true)
+print ("mae error is: " + str(mae_val))
+
