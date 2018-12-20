8ad05540131ebb9448b71b90af797e3af17dd139
zjk0803 <43437186+zjk0803@users.noreply.github.com>
2018-12-20 12:17:24 +0800

    Update pythonic.py

--- pythonic.py
+++ pythonic.py
@@ -1,1 +1,21 @@
 for i in range(len(lst)):
+  print(lst([i])
+
+for i in lst:
+  print(i)
+
+        
+        
+temp = a
+a = b
+b = temp
+
+        
+a, b = b, a
+
+        
+        
+        
+def func(a, b):
+  result =         
+        
