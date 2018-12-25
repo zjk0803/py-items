34e4eee763b5a775966e02251a8a109c8c9d2883
zjk0803 <43437186+zjk0803@users.noreply.github.com>
2018-12-25 12:28:33 +0800

    Update scrapy.py

--- scrapy.py
+++ scrapy.py
@@ -2,4 +2,8 @@ import requests
 
 result = requests.get('http://quotes.toscrape.com/')
 page = result.text
+from selenium import webdriver
+driver = webriver.Chrome()
+driver.get('http://quotes.toscrape.com/')
+page = driver.page_source
 
