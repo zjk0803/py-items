97776a53fdbc5a3687ccc66162d85f249478244a
zjk0803 <43437186+zjk0803@users.noreply.github.com>
2018-12-30 23:53:36 +0800

    Update OCR.py

--- OCR.py
+++ OCR.py
@@ -1,6 +1,7 @@
 #图片识别
 
-#Tesseract的Windows安装包下载地址为：http://digi.bib.uni-mannheim.de/tesseract/tesseract-ocr-setup-4.00.00dev.exe ，下载后双击直接安装即可。安装完后，需要将Tesseract添加到系统变量中。在CMD中输入tesseract -v, 
+#Tesseract的Windows安装包下载地址为：http://digi.bib.uni-mannheim.de/tesseract/tesseract-ocr-setup-4.00.00dev.exe ，
+#下载后双击直接安装即可。安装完后，需要将Tesseract添加到系统变量中。在CMD中输入tesseract -v, 
 import pytesseract
 from PIL import Image
 
